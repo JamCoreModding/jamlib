4c00f2ca3f2735d51d6ab7d9e7a66c9546f61485
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-05-06 08:05:48 +0100

    feat: tick scheduling

--- src/main/java/io/github/jamalam360/jamlib/JamLib.java
+++ src/main/java/io/github/jamalam360/jamlib/JamLib.java
@@ -1,10 +1,45 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Jamalam
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package io.github.jamalam360.jamlib;
 
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class JamLib {
+public class JamLib implements ModInitializer {
     public static Logger getLogger(String subName) {
         return LogManager.getLogger("JamLib/" + subName);
+    }
+
+    @Override
+    public void onInitialize() {
+        ClientTickEvents.END_WORLD_TICK.register(TickScheduling::onEndTickClient);
+        ServerTickEvents.END_WORLD_TICK.register(TickScheduling::onEndTickServer);
+
+        getLogger("JamLibInit").info("JamLib has been initialized");
     }
 }
