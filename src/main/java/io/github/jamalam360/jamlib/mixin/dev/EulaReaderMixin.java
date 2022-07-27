cbd520ab5e299b9ebc5fee438195cba346b835c3
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-07-27 15:22:48 +0100

    feat: make eula always agreed to in dev

---
+++ src/main/java/io/github/jamalam360/jamlib/mixin/dev/EulaReaderMixin.java
@@ -0,0 +1,23 @@
+package io.github.jamalam360.jamlib.mixin.dev;
+
+import net.minecraft.server.dedicated.EulaReader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * @author Jamalam
+ */
+
+@Mixin(EulaReader.class)
+public class EulaReaderMixin {
+    @Inject(
+            method = "isEulaAgreedTo",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    public void jamlib$setEulaAlwaysAgreedToInDev(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
