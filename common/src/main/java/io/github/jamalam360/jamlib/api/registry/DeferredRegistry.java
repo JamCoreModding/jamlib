e65df715f52e808279cd21a68e4e286b8a572b11
Jamalam <james@jamalam.tech>
2026-05-12 19:05:33 +0100

    feat: javadoc for registries

--- common/src/main/java/io/github/jamalam360/jamlib/api/registry/DeferredRegistry.java
+++ common/src/main/java/io/github/jamalam360/jamlib/api/registry/DeferredRegistry.java
@@ -11,7 +11,20 @@ import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * Cross-platform deferred registry system.
+ *
+ * @param <T> The registry type.
+ */
 public class DeferredRegistry<T> {
+	/**
+	 * Creates a new deferred registry.
+	 *
+	 * @param modId The mod ID, used for creating identifiers.
+	 * @param registry The registry.
+	 * @param <T> The type of objects within the registry.
+	 * @return A new deferred registry.
+	 */
 	public static <T> DeferredRegistry<T> create(String modId, Registry<T> registry) {
 		return new DeferredRegistry<>(modId, registry);
 	}
@@ -26,24 +39,51 @@ public class DeferredRegistry<T> {
 		this.queue = new HashMap<>();
 	}
 
+	/**
+	 * Queues an object for registration.
+	 * @param path The identifier path for the object.
+	 * @param object The object supplier.
+	 * @return A registry object holding the object.
+	 */
 	public RegistryObject<T> register(String path, Supplier<T> object) {
 		return this.register(Identifier.fromNamespaceAndPath(this.modId, path), (ignored) -> object.get());
 	}
 
+	/**
+	 * Queues an object for registration.
+	 * @param identifier The identifier for the object.
+	 * @param object The object supplier.
+	 * @return A registry object holding the object.
+	 */
 	public RegistryObject<T> register(Identifier identifier, Supplier<T> object) {
 		return this.register(identifier, (ignored) -> object.get());
 	}
 
+	/**
+	 * Queues an object for registration.
+	 * @param path The identifier path for the object.
+	 * @param object The object supplier.
+	 * @return A registry object holding the object.
+	 */
 	public RegistryObject<T> register(String path, Function<ResourceKey<T>, T> object) {
 		return this.register(Identifier.fromNamespaceAndPath(this.modId, path), object);
 	}
 
+	/**
+	 * Queues an object for registration.
+	 * @param identifier The identifier for the object.
+	 * @param object The object supplier.
+	 * @return A registry object holding the object.
+	 */
 	public RegistryObject<T> register(Identifier identifier, Function<ResourceKey<T>, T> object) {
 		RegistryObject<T> registryObject = new MemoizedRegistryObject<>(object);
 		this.queue.put(identifier, registryObject);
 		return registryObject;
 	}
 
+	/**
+	 * Registers all queued objects. This should be called during mod initialization.
+	 */
 	public void registerEntries() {
 		PlatformRegistries.submitRegistryEntries(this.registry, this.queue);
 	}
--- common/src/main/java/io/github/jamalam360/jamlib/api/registry/RegistryObject.java
+++ common/src/main/java/io/github/jamalam360/jamlib/api/registry/RegistryObject.java
@@ -4,6 +4,15 @@ import net.minecraft.resources.ResourceKey;
 
 import java.util.function.Supplier;
 
+/**
+ * An object within a registry.
+ *
+ * @param <T> The registry type.
+ */
 public interface RegistryObject<T> extends Supplier<T> {
+	/**
+	 * @param key The registry key of the object being created.
+	 * @return The created object.
+	 */
 	T create(ResourceKey<T> key);
 }
