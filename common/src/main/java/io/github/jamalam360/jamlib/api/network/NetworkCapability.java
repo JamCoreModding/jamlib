80b615691fe379751f7cd858dad22bd82cd118ad
Jamalam <james@jamalam.tech>
2026-05-28 08:00:18 +0100

    docs: javadocs for network capabilities

--- common/src/main/java/io/github/jamalam360/jamlib/api/network/NetworkCapability.java
+++ common/src/main/java/io/github/jamalam360/jamlib/api/network/NetworkCapability.java
@@ -1,5 +1,12 @@
 package io.github.jamalam360.jamlib.api.network;
 
+/**
+ * Represents the network capabilities of a server or a connected client.
+ */
 public interface NetworkCapability {
+	/**
+	 * @param type The type of payload to check for.
+	 * @return {@code true} if the connection that this capability represents can handle the packet type.
+	 */
 	boolean canReceive(PayloadType<?> type);
 }
