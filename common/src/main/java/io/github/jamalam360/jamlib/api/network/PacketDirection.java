125058cda40bd6f35813a09291c60a7f0eac89fd
Jamalam <james@jamalam.tech>
2026-06-10 10:39:04 +0100

    docs: finish docs

--- common/src/main/java/io/github/jamalam360/jamlib/api/network/PacketDirection.java
+++ common/src/main/java/io/github/jamalam360/jamlib/api/network/PacketDirection.java
@@ -1,5 +1,8 @@
 package io.github.jamalam360.jamlib.api.network;
 
+/**
+ * The direction of a packet, either serverbound or clientbound.
+ */
 public enum PacketDirection {
 	SERVERBOUND,
 	CLIENTBOUND
